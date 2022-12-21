15b086884e10d9a8b87be80700ef873093b7c4bd
Adwait <adwait.kul.2018@gmail.com>
2022-12-21 08:40:21 -0800

    only following terms

--- src/main/java/com/adwaitkulkarni58/twitter/kafka/project/kafka/producer/TwitterProducer.java
+++ src/main/java/com/adwaitkulkarni58/twitter/kafka/project/kafka/producer/TwitterProducer.java
@@ -35,9 +35,7 @@ public class TwitterProducer {
 		Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
 		StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
 		// Optional: set up some followings and track terms
-		List<Long> followings = Lists.newArrayList(1234L, 566788L);
-		List<String> terms = Lists.newArrayList("twitter", "api");
-		hosebirdEndpoint.followings(followings);
+		List<String> terms = Lists.newArrayList("kafka");
 		hosebirdEndpoint.trackTerms(terms);
 
 		// These secrets should be read from a config file
