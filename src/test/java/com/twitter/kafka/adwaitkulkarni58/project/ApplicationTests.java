8f46608e7c3757dc001d8241850b577c4ea59827
Adwait <adwait.kul.2018@gmail.com>
2022-12-20 17:40:03 -0800

    First commit

---
+++ src/test/java/com/twitter/kafka/adwaitkulkarni58/project/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitter.kafka.adwaitkulkarni58.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
