ab9f8c2d9c4591634d03a580fdf4ab9ae8cff57d
Adwait <adwait.kul.2018@gmail.com>
2022-12-20 18:52:51 -0800

    Create blank methods

--- src/main/java/com/adwaitkulkarni58/twitter/kafka/project/kafka/producer/TwitterProducer.java
+++ src/main/java/com/adwaitkulkarni58/twitter/kafka/project/kafka/producer/TwitterProducer.java
@@ -2,4 +2,19 @@ package com.adwaitkulkarni58.twitter.kafka.project.kafka.producer;
 
 public class TwitterProducer {
 
+	// constructor
+	public TwitterProducer() {
+
+	}
+
+	// run the producer
+	public void run() {
+
+	}
+
+	// create twitter client
+	public void createTwitterClient() {
+
+	}
+
 }
