1c3b9abd98cf948cbdd6c84b002568bcc22302f2
Adwait <adwait.kul.2018@gmail.com>
2022-12-21 08:37:33 -0800

    declare the host to connect to from hbc documentation

--- src/main/java/com/adwaitkulkarni58/twitter/kafka/project/kafka/producer/TwitterProducer.java
+++ src/main/java/com/adwaitkulkarni58/twitter/kafka/project/kafka/producer/TwitterProducer.java
@@ -1,7 +1,16 @@
 package com.adwaitkulkarni58.twitter.kafka.project.kafka.producer;
 
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+
+import com.google.common.collect.Lists;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.Hosts;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
 
 public class TwitterProducer {
 
@@ -18,6 +27,21 @@ public class TwitterProducer {
 	// create twitter client
 	public void createTwitterClient() {
 		BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
+
+		/**
+		 * Declare the host you want to connect to, the end point, and authentication
+		 * (basic auth or oauth)
+		 */
+		Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+		StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
+		// Optional: set up some followings and track terms
+		List<Long> followings = Lists.newArrayList(1234L, 566788L);
+		List<String> terms = Lists.newArrayList("twitter", "api");
+		hosebirdEndpoint.followings(followings);
+		hosebirdEndpoint.trackTerms(terms);
+
+		// These secrets should be read from a config file
+		Authentication hosebirdAuth = new OAuth1("consumerKey", "consumerSecret", "token", "secret");
 	}
 
 }
