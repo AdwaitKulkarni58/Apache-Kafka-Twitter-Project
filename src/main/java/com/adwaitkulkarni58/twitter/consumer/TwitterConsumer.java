90b18b2a2f74c08951794da7f1e0885d269c1471
Adwait <adwait.kul.2018@gmail.com>
2022-12-22 18:08:12 -0800

    Completed creating a consumer

--- src/main/java/com/adwaitkulkarni58/twitter/consumer/TwitterConsumer.java
+++ src/main/java/com/adwaitkulkarni58/twitter/consumer/TwitterConsumer.java
@@ -1,16 +1,20 @@
 package com.adwaitkulkarni58.twitter.consumer;
 
 import java.time.Duration;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Properties;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TwitterConsumer {
+
+	Logger logger = LoggerFactory.getLogger(getClass());
 
 	public void createConsumer() {
 		String BOOTSTRAP_SERVERS = "127.0.0.1:9092";
@@ -23,11 +27,19 @@ public class TwitterConsumer {
 		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 
-		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+		@SuppressWarnings("resource")
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
 
 		consumer.subscribe(Collections.singleton(topic));
 
-		ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+		while (true) {
+			ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+			for (ConsumerRecord<String, String> record : records) {
+				logger.info("Key:" + record.key() + ", value:" + record.value());
+			}
+		}
+
 	}
 
 }
