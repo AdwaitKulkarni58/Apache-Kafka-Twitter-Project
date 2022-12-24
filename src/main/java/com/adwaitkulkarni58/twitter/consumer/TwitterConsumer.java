b3e09399f33e1dac3bff40fd8ee6860bc89f8639
Adwait <adwait.kul.2018@gmail.com>
2022-12-23 18:09:49 -0800

    Add elastic search config

--- src/main/java/com/adwaitkulkarni58/twitter/consumer/TwitterConsumer.java
+++ src/main/java/com/adwaitkulkarni58/twitter/consumer/TwitterConsumer.java
@@ -4,17 +4,45 @@ import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
 
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestClientBuilder.HttpClientConfigCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TwitterConsumer {
 
 	Logger logger = LoggerFactory.getLogger(getClass());
+
+	public RestClient createClient() {
+
+		final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+		credentialsProvider.setCredentials(AuthScope.ANY,
+				new UsernamePasswordCredentials("user", "test-user-password"));
+
+		RestClientBuilder builder = RestClient.builder(new HttpHost(hostname, 443))
+				.setHttpClientConfigCallback(new HttpClientConfigCallback() {
+					@Override
+					public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+						return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+					}
+				});
+
+		RestClient restClient = new RestClient(builder);
+
+		return restClient;
+	}
 
 	public void createConsumer() {
 		String BOOTSTRAP_SERVERS = "127.0.0.1:9092";
