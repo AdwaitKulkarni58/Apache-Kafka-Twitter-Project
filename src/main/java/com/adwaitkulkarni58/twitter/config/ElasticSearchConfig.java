7b9602c13bc514e901378da7771e0d78a531c274
Adwait <adwait.kul.2018@gmail.com>
2022-12-23 18:29:00 -0800

    Created config file for elasticsearch credentials

---
+++ src/main/java/com/adwaitkulkarni58/twitter/config/ElasticSearchConfig.java
@@ -0,0 +1,32 @@
+package com.adwaitkulkarni58.twitter.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:elasticsearch.properties")
+public class ElasticSearchConfig {
+
+	@Value("${elasticsearch.hostname}")
+	private String hostname;
+
+	@Value("${elasticsearch.username}")
+	private String username;
+
+	@Value("${elasticsearch.password}")
+	private String password;
+
+	public String getHostname() {
+		return hostname;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+}
